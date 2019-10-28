9c493d73da12bdf67d1de32bd26f4f0573518fa8
decocas <37914111+decocas@users.noreply.github.com>
2019-10-28 13:52:02 -0500

    Add files via upload

--- server/control/control.js
+++ server/control/control.js
@@ -7,30 +7,39 @@ e.getEmpleado= async (req, res)=>{
 e.Crear=  async (req, res)=>{
   const e=new empleado(req.body);
  await e.save();
- 
+ console.log(req.body);
  res.json('ok');
 };
 e.getId = async (req,res)=>{
-  const emp= await empleado.findById(req.params.id);
+  const emp= await empleado.findById(req.params.id,(err,doc)=>{
+    if(err){
+      console.log('not ok');
+    }
+    console.log(doc);
+  });
      res.json(emp); 
 };
+ 
 e.Editar= async (req, res) =>{
-  //const {id } =req.params; // es lo mismo que (req.params.id) que esta en getId
+  
+ //const {id } =req.params; // es lo mismo que (req.params.id) que esta en getId
   const emp={name: req.body.name,
              salary: req.body.salary, 
              status: req.body.status 
   } ;
-  const e= await empleado.findOneAndUpdate(req.params.id,{ $set: emp }, {new: true},(err, doc)=>{
+   await empleado.updateOne(/*req.params.id*/ {"_id": req.params.id},/*id,*/{ $set: emp },{upsert:true},(err, doc)=>{
     if(err){
       console.log('not ok');
     }
+    console.log(empleado);
     console.log(doc);
     
   }); // buscar y si no lo encuentra que lo cree  
    // ojo pq si no el json te manda error cuando se inseta se coloca 'status': emp}
  
    res.json({status: 'ok'});}
-
+ 
+ 
 e.Eliminar= async (req, res)=>{
 await empleado.findOneAndDelete(req.params.id);
 res.json({status: ' edelet ok'});
--- server/dd.js
+++ server/dd.js
@@ -2,5 +2,6 @@ var db= require('mongoose');
 var url= 'mongodb://localhost/ejemplo';
  db.set('useNewUrlParser', true);
  db.set('useFindAndModify',false);
+ db.set('useUnifiedTopology', true);
 db.connect(url,{ useFindAndModify: false }).then(data=>console.log('db conectada')).catch(err=>console.log(err));
 db.connect(url,{useNewUrlParser: true}).then(data=>console.log('db conectada')).catch(err=>console.log(err));
